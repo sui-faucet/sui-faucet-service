fe6e1830a545e3ef8ab7327c9fbf77ec865f8c32
Le Khac Thanh Tung <lekhacthanhtung.it@gmail.com>
2025-07-16 19:48:49 +0700

    feat: add redis (#1)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  app.setGlobalPrefix('api');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
