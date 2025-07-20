e8d18a1ccee6ab20601c4995a686f661caaf609a
Le Khac Thanh Tung <lekhacthanhtung.it@gmail.com>
2025-07-20 08:53:16 +0700

    feat: add health module (#5)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose'
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SuiModule } from './sui/sui.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
@@ -11,6 +13,8 @@ import { SuiModule } from './sui/sui.module';
       isGlobal: true,
       envFilePath: '.env',
     }),
+    HealthModule,
+    MongooseModule.forRoot(process.env.MONGODB_CONNECTION_STRING || ''),
     SuiModule,
   ],
   controllers: [AppController],
